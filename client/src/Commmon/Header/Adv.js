c2c924101792ce5c385a95775eaffaa89d23a11c
km Habib <km.habibs@gmail.com>
2024-01-26 16:16:09 +0600

    Adv design done

--- client/src/Commmon/Header/Adv.js
+++ client/src/Commmon/Header/Adv.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Adv() {
-  return <div>Adv</div>;
+  return (
+    <div className="hidden md:block">
+      <div className="h-64 bg-black flex items-center justify-center flex-col">
+        <img src="/images/Header/adv.jpg" alt="adv" />
+        <p className="text-zinc-600  text-xs self-start mt-1 ml-15%">
+          Advertisement
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Adv;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "15%": "15%", // ml-15%
+      },
+    },
   },
   plugins: [],
 };
