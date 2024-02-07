df888b4eaf1b61a08115bd1b347437d8e6a9a5f3
km Habib <km.habibs@gmail.com>
2024-02-07 15:51:57 +0600

    Adding Input field for Creating News Article

--- client/src/Admin/Admin.js
+++ client/src/Admin/Admin.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import React from "react";
 import Sidebar from "./Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Header from "./Header";
+import CreateNews from "./CreateNews";
 
 function Admin() {
   axios
@@ -18,10 +22,19 @@ function Admin() {
     });
 
   return (
-    <div className="flex flex-col min-h-screen max-h-full dashboard w-full">
+    <div
+      className="flex flex-col min-h-screen max-h-full dashboard w-full"
+      style={{ backgroundColor: "#f1f5f9" }}>
       <div className="flex">
         <Sidebar />
-        <div></div>
+        <div className="flex-grow flex flex-col">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/news-management/create" element={<CreateNews />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
---
+++ client/src/Admin/CreateNews.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+
+function CreateNews() {
+  const [title, setTitle] = useState("");
+  const [types, setTypes] = useState([]);
+  const [NewsCategory, setNewsCategory] = useState([]);
+  const [NewsSubCategory, setNewsSubCategory] = useState([]);
+  const [selectedType, setSelectedType] = useState("");
+  const [selectedNewsCategory, setSelectedNewsCategory] = useState("");
+  const [selectedNewsSubCategory, setSelectedNewsSubCategory] = useState("");
+  const [authorName, setAuthorName] = useState("");
+  const handleNewsTypeChange = (e) => {
+    setSelectedType(e.target.value);
+    console.log(e.target.value);
+  };
+  const handleNewsCategoryChange = (e) => {
+    setSelectedNewsCategory(e.target.value);
+    console.log(e.target.value);
+  };
+  const handleNewsSubCategoryChange = (e) => {
+    setSelectedNewsSubCategory(e.target.value);
+    console.log(e.target.value);
+  };
+  const handleFileChange = (e) => {
+    console.log(e.target.value);
+  };
+  const handleSubmit = async () => {};
+  return (
+    <div className="mt-28">
+      <div className="mx-auto bg-white drop-shadow-md w-10/12 rounded">
+        <h3 className="p-6 font-bold mb-8 text-black border-b">
+          {" "}
+          Write News Article
+        </h3>
+        <div className="container p-5">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-10">
+              <label htmlFor="title" className="block text-sm  text-gray-600">
+                Title
+              </label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="mt-2 p-3 bg-gray-200 focus:outline-none w-full border rounded-md"
+              />
+            </div>
+            <div className="mb-10">
+              <label htmlFor="newsType" className="block text-sm text-gray-600">
+                News Type
+              </label>
+              <select
+                id="NewsType"
+                name="NewsType"
+                value={selectedType}
+                onChange={handleNewsTypeChange}
+                className="mt-2 p-3 bg-gray-200 focus:outline-none w-full border rounded-md">
+                <option value="" disabled>
+                  Select News Type
+                </option>
+                {types.map((types) => (
+                  <option value={types.name} key={types._id}>
+                    {types.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="mb-10">
+              <label className="block text-sm text-gray-600">Upload File</label>
+              <input
+                type="file"
+                name="file"
+                id="file"
+                onChange={handleFileChange}
+                className="sr-only"
+                accept="image/*,video/*"
+              />
+              <label
+                htmlFor="file"
+                className="mt-1 p-2 w-full cursor-pointer border border-gray-300 rounded-md flex items-center  justify-center bg-blue-600 text-white hover:bg-gray-900">
+                Select Image or Video
+              </label>
+            </div>
+            <div className="mb-10">
+              <label htmlFor="newsType" className="block text-sm text-gray-600">
+                News Category
+              </label>
+              <select
+                id="NewsCategory"
+                name="NewsCategory"
+                value={selectedNewsCategory}
+                onChange={handleNewsCategoryChange}
+                className="mt-2 p-3 bg-gray-200 focus:outline-none w-full border rounded-md">
+                <option value="" disabled>
+                  Select News Category
+                </option>
+                {NewsCategory.map((category) => (
+                  <option value={category.name} key={category._id}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="mb-10">
+              <label htmlFor="newsType" className="block text-sm text-gray-600">
+                News Sub Category
+              </label>
+              <select
+                id="NewsSubCategory"
+                name="NewsSubCategory"
+                value={selectedNewsSubCategory}
+                onChange={handleNewsSubCategoryChange}
+                className="mt-2 p-3 bg-gray-200 focus:outline-none w-full border rounded-md">
+                <option value="" disabled>
+                  Select News Sub Category
+                </option>
+                {NewsSubCategory.map((subcategory) => (
+                  <option value={subcategory.name} key={subcategory._id}>
+                    {subcategory.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="mb-10">
+              <label htmlFor="author" className="block text-sm  text-gray-600">
+                Author
+              </label>
+              <input
+                type="text"
+                id="author"
+                name="author"
+                value={authorName}
+                onChange={(e) => setAuthorName(e.target.value)}
+                className="mt-2 p-3 bg-gray-200 focus:outline-none w-full border rounded-md"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateNews;
